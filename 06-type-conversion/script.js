674e871860f6e208de37c35e77cad8462f304a3c
alexangria1992 <alex040892@gmail.com>
2024-01-19 16:01:50 +1300

    Type Conversion

---
+++ 06-type-conversion/script.js
@@ -0,0 +1,25 @@
+let amount = "hello";
+
+// Change String to number
+amount = parseInt(amount);
+
+// amount = parseInt(amount);
+
+// amount = Number(amount);
+
+//Change number to string
+// amount = amount.toString();
+// amount = String(amount);
+
+//Change string to decimal
+// amount = parseFloat(amount);
+
+// Convert number to boolean
+
+console.log(Math.sqrt(-1));
+console.log(1 + NaN);
+console.log(undefined + undefined);
+console.log("foo" / 3);
+
+// amount = Boolean(amount);
+console.log(amount, typeof amount);
