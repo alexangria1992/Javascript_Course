09861e6ef924c6ea60fc2c29ed781c61fce36c0b
alexangria1992 <alex040892@gmail.com>
2024-01-20 21:26:32 +1300

    Destructuring & Naming

---
+++ 02-arrays-and-objects/07-destructuring-and-naming/script.js
@@ -0,0 +1,36 @@
+const firstName = "John";
+const lastName = "Doe";
+const age = 30;
+
+const person = {
+  firstName,
+  lastName,
+  age,
+};
+
+console.log(person.age);
+
+// Destructuring
+
+const todo = {
+  id: 1,
+  title: "take out trash",
+  user: {
+    name: "John",
+  },
+};
+
+const {
+  id: todoId,
+  title,
+  user: { name },
+} = todo;
+
+console.log(todoId);
+
+//Destructuring Arrays
+const numbers = [23, 67, 33, 49, 52];
+
+const [first, second, ...nums] = numbers;
+
+console.log(first, second, nums);
