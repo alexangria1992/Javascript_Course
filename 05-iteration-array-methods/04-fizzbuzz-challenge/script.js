648dd2a47844030628c8ae6cc145c68be9bdb38e
alexangria1992 <alex040892@gmail.com>
2024-01-26 11:35:17 +1300

    FizzBuzz challenge

---
+++ 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -0,0 +1,31 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
+  }
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// While Loop
+
+let j = 1;
+
+while (j <= 100) {
+  if (j % 5 === 0 && j % 3 === 0) {
+    console.log("FizzBuzz");
+  }
+  if (j % 3 === 0) {
+    console.log("Fizz");
+  } else if (j % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(j);
+  }
+
+  j++;
+}
