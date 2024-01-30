53e4d094ffbddf4653aa7a37ad8a270e61445a51
alexangria1992 <alex040892@gmail.com>
2024-01-30 16:55:26 +1300

    Intro to the Dom

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,14 @@
+// console.log(window.document);
+
+// console.log(document.body.innerHTML);
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = "<h1>Hello Worldd</h1>";
+
+// document.write("Hello from JS");
+
+console.log(document.getElementById("main"));
+const main = document.getElementById("main");
+// main.innerHTML = "<h1>Hello from main</h1>";
+document.querySelector("#main h1").innerHTML = "Hello";
