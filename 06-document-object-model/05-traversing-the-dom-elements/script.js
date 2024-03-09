b072b580211976bd9ee16b579145eda509f27c31
Alexandre Angrignon <alex040892@gmail.com>
2024-03-09 19:31:21 +1300

    Traversing the DOM - All Nodes

--- 06-document-object-model/05-traversing-the-dom-elements/script.js
+++ 06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -1,5 +1,5 @@
 // Get child elements
-
+let output;
 const parent = document.querySelector(".parent");
 output = parent.children;
 output = parent.children[1];
@@ -15,19 +15,41 @@ parent.lastElementChild.innerText = "child three";
 
 // Get parent elements
 
-const child = document.querySelector(".child");
+// const child = document.querySelector(".child");
 
-output = child.parentElement;
-child.parentElement.style.border = "1px solid #ccc";
-child.parentElement.style.padding = "10px";
+// output = child.parentElement;
+// child.parentElement.style.border = "1px solid #ccc";
+// child.parentElement.style.padding = "10px";
 
 //Sibling Elements
+// const secondItem = document.querySelector(".child:nth-child(2)");
+// output = secondItem;
+// output = secondItem.nextElementSibling;
+
+// secondItem.nextElementSibling.style.color = "green";
+// secondItem.previousElementSibling.style.color = "yellow";
+
+// All Nodes
+output = parent.childNodes;
+output = parent.childNodes[0].textContent;
+output = parent.childNodes[0].nodeName;
+output = parent.childNodes[3].textContent;
+output = parent.childNodes[3].outerHTML;
+output = parent.childNodes[5].style.color = "red";
+output = parent.firstChild;
+output = parent.lastChild;
+parent.lastChild.textContent = "helllo";
+
+// Parent node
+const child = document.querySelector(".child");
+output = child.parentNode;
+output = child.parentElement;
+
+child.parentNode.style.backgroundColor = "#ccc";
+child.parentNode.style.padding = "10px";
+
+// siblings
 const secondItem = document.querySelector(".child:nth-child(2)");
-output = secondItem;
-output = secondItem.nextElementSibling;
-
-secondItem.nextElementSibling.style.color = "green";
-secondItem.previousElementSibling.style.color = "yellow";
-
+output = secondItem.nextSibling;
 console.log(output);
 // console.dir(output);
